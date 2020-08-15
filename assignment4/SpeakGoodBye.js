422af60b11a23fd8ca4dfc24625f41c7b46b7039
Nishit Sethi <sethi.nishit@gmail.com>
2020-08-15 20:11:01 +0530

    Good

---
+++ assignment4/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window){
+	var byeSpeaker={};
+	byeSpeaker.word="Good Bye";
+	byeSpeaker.speak=function(name){
+		console.log(byeSpeaker.word + " " + name);
+	}
+	window.byeSpeaker=byeSpeaker;
+})(window);
---
+++ assignment4/SpeakHello.js
@@ -0,0 +1,9 @@
+
+(function (window){
+	var helloSpeaker={};
+	helloSpeaker.word="Hello";
+	helloSpeaker.speak=function(name){
+		console.log(helloSpeaker.word + " " + name);
+	}
+	window.helloSpeaker=helloSpeaker;
+})(window);
